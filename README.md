6218d820e34ab63a3603bdf0cb9ddeb8916e3172
Alexandru Vladutu <alessio.ijoomla@gmail.com>
2012-12-09 12:54:00 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,87 @@
+      __                                        __
+     /\ \                                      /\ \__
+     \_\ \  __  __    ___   _ __   ___   __  __\ \ ,_\    __
+     /'_` \/\ \/\ \ /' _ `\/\`'__\/ __`\/\ \/\ \\ \ \/  /'__`\
+    /\ \L\ \ \ \_\ \/\ \/\ \ \ \//\ \L\ \ \ \_\ \\ \ \_/\  __/
+    \ \___,_\/`____ \ \_\ \_\ \_\\ \____/\ \____/ \ \__\ \____\
+     \/__,_ /`/___/> \/_/\/_/\/_/ \/___/  \/___/   \/__/\/____/
+                /\___/
+                \/__/
+
+  Node.js command line tool for dynamically updating your domain's dns to your current ip using [Amazon Route53](http://aws.amazon.com/route53/).
+
+```bash
+dynroute -h
+
+Usage: dynroute [options]
+
+Options:
+   -d, --domain          Domain to update [required]
+   -ttl, --ttl           Time To Leave (in seconds) [default: 60]
+   -t, --time            How frequently to check for IP update [default: 60]
+   -o, --once            Check for IP update ONLY ONCE and exit [default: false]
+   -g, --growl           Display growl notification after IP update [default: false]
+   -aws, --awskeys       The path for the config file containing AWS credentials [default ~/.awsrc]
+   -ipserv, --ipserver   Resp: { ip: "YOUR_IP_HERE" } [default http://whatismyip.nodejitsu.com/index.json]
+   -dbg, --debug         Print debugging info
+   -v, --version         Print version and exit
+```
+
+## Installation
+
+    $ npm install -g dynroute
+
+## Usage
+
+  - Create a hosted zone for your domain in Route53, in case you don't already have one.
+  - Update your domain's nameservers with the ones provided by Route53 (for that hosted zone).
+  - Put your credentials into a config file such as ~/.awsrc (used by default)
+
+```
+[credentials]
+accessKeyId     = AAAAAAAAAAAAAAAAAAAA
+secretAccessKey = bbbbbbbbbbbbbbbbbbbbbLwccccembeMdddddI4e
+```
+
+### Update now & exit
+
+```bash
+dyndns -d home.mydomain.com --once
+```
+
+### Leave script open to check for updated every X seconds
+
+```bash
+dyndns -d home.mydomain.com
+```
+
+By default the script will check every 60 seconds if your ip updated, but you can customize that:
+
+```bash
+dyndns -d home.mydomain.com --time 120
+```
+
+## License
+
+>(The MIT License)
+>
+>Copyright (c) 2012 Alexandru Vl&#259;du&#355;u &lt;alexandru.vladutu@gmail.com&gt;
+>
+>Permission is hereby granted, free of charge, to any person obtaining
+>a copy of this software and associated documentation files (the
+>'Software'), to deal in the Software without restriction, including
+>without limitation the rights to use, copy, modify, merge, publish,
+>distribute, sublicense, and/or sell copies of the Software, and to
+>permit persons to whom the Software is furnished to do so, subject to
+>the following conditions:
+>
+>The above copyright notice and this permission notice shall be
+>included in all copies or substantial portions of the Software.
+>
+>THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+>EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+>MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+>IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+>CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+>TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+>SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ dynroute.js
@@ -0,0 +1,235 @@
+var awssum          = require('awssum'),
+    amazon          = awssum.load('amazon/amazon'),
+    Route53         = awssum.load('amazon/route53').Route53,
+    _               = require('underscore'),
+    async           = require('async'),
+    fmt             = require('fmt'),
+    utils           = require('./lib/utils'),
+    growl           = require('growl'),
+    request         = require('request'),
+    env             = process.env,
+    lastKnownIP, hostedZones, opts, r53;
+
+function searchRecord(ip) {
+  r53.ListHostedZones(function(err, data) {
+    if (err) {
+      if (opts.debug) {
+        utils.debugBlock('Error while listing hosted zones', err);
+      }
+      return;
+    }
+
+    hostedZones = data.Body.ListHostedZonesResponse.HostedZones.HostedZone;
+
+    // dirty little hack for next map function
+    if (!utils.isArray(hostedZones)) { hostedZones = [hostedZones]; }
+
+    hostedZones = _.map(hostedZones, function(zone) {
+      return {
+        id   : zone.Id.replace('/hostedzone/', ''),
+        name : zone.Name.slice(0, zone.Name.length - 1)
+      };
+    });
+
+    if (opts.debug) {
+      utils.debugBlock('Hosted Zones', hostedZones);
+    }
+
+    async.map(hostedZones, function(zone, callback) {
+      r53.ListResourceRecordSets({ HostedZoneId : zone.id }, function(err, data) {
+        var resp, records;
+
+        if (err) { return callback(err); }
+
+        records = data.Body.ListResourceRecordSetsResponse.ResourceRecordSets.ResourceRecordSet;
+        records = _.filter(records, function(record) {
+          return record.Type === 'A';
+        });
+
+        resp = {
+          id      : zone.id,
+          records : records
+        };
+
+        callback(null, resp);
+      });
+    }, function(err, zones) {
+      if (err) {
+        if (opts.debug) {
+          utils.debugBlock('Error listing resource record sets', err);
+        }
+        return;
+      }
+
+      if (opts.debug) {
+        utils.debugBlock('Zones with A records', zones);
+      }
+
+      (function iterate(iterator) {
+        var zone, i, len, record, rootZone;
+
+        zone = zones[iterator];
+
+        // current zone has A type records, iterate through them until
+        // domain is found
+        if (zone.records.length) {
+          // TODO: make simple forEach async
+          for (i = 0, len = zone.records.length; i < len; i++) {
+            if (zone.records[i].Name === (opts.domain + '.')) {
+              return updateDns(ip, {
+                zoneId : zone.id,
+                record : zone.records[i]
+              });
+            }
+          }
+        }
+
+        // proceed to next zone from the array
+        if (iterator) {
+          process.nextTick(function() {
+            iterate(--iterator);
+          });
+        } else {
+          if (opts.debug) {
+            utils.debugBlock("Record doesn't exist, create it", hostedZones);
+          }
+          // no such record was found, iterate through hostedZones
+          // and check for the root domain
+
+          hostedZones.forEach(function(zone) {
+            if (opts.domain.indexOf(zone.name)) {
+              rootZone = zone;
+            }
+          });
+
+          return updateDns(ip, {
+            zoneId : zone.id
+          });
+        }
+      }(zones.length - 1));
+    });
+
+  });
+}
+
+function updateDns(ip, details) {
+  var args, oldIps;
+
+  args = {
+    HostedZoneId: '/' + details.zoneId,
+    Comment: 'Updated local ip at: ' + Date.now()
+  };
+
+  // if record already exists in Route53 - delete & re-create it
+  if (details.record) {
+    oldIps = details.record.ResourceRecords.ResourceRecord;
+    // dirty little hack for next map function
+    if (!utils.isArray(oldIps)) { oldIps = [oldIps]; }
+    oldIps = _.map(oldIps, function(ip) {
+      return ip.Value;
+    });
+
+    // IP still the same, no change needed
+    if (oldIps.length === 1 && oldIps[0] === ip) {
+      lastKnownIP = ip;
+
+      if (opts.debug) {
+        utils.debugBlock('Ip unchanged', 'No request sent!')
+      }
+
+      return;
+    }
+
+    args.Changes = [
+      {
+        Action          : 'DELETE',
+        Name            : opts.domain,
+        Type            : 'A',
+        Ttl             : details.record.TTL,
+        ResourceRecords : oldIps
+      },
+      {
+        Action          : 'CREATE',
+        Name            : opts.domain,
+        Type            : 'A',
+        Ttl             : opts.ttl,
+        ResourceRecords : [ip]
+      }
+    ];
+  } else {
+    // record doesn't exist in Route53, create it
+    args.Changes = [
+      {
+        Action          : 'CREATE',
+        Name            : opts.domain,
+        Type            : 'A',
+        Ttl             : opts.ttl,
+        ResourceRecords : [ip]
+      }
+    ];
+  }
+
+  if (opts.debug) {
+    utils.debugBlock('Sent ip change request to Route53', args);
+  }
+
+  r53.ChangeResourceRecordSets(args, function(err, data) {
+    if (err) {
+      if (opts.debug) {
+        utils.debugBlock('Error while updating ip in Route53', err);
+      }
+      return;
+    }
+
+    if (opts.debug) {
+      utils.debugBlock('ChangeResourceRecordSetsResponse', data);
+    }
+
+    if (!err && data &&
+      ((data.Body.ChangeResourceRecordSetsResponse.ChangeInfo.Status === 'PENDING') ||
+      (data.Body.ChangeResourceRecordSetsResponse.ChangeInfo.Status === 'CREATE'))) {
+
+      if (opts.debug) {
+        utils.debugBlock('IP change successful', 'Sending growl notification');
+      }
+
+      if (opts.growl) {
+        growl(opts.domain + ' : ' + ip, { title: 'Dynamic DNS Update' });
+      }
+
+      lastKnownIP = ip;
+    }
+  });
+}
+
+function checkIpUpdate(credentials, configs) {
+  opts = configs;
+
+  // initialize once
+  r53 = r53 || new Route53(credentials);
+
+  request(opts.ipserver, function (error, response, body) {
+    var ip;
+
+    if (!error && response.statusCode == 200) {
+      try {
+        ip = JSON.parse(body.trim()).ip;
+        // no point in querying Route53 if we know the last ip is correct right?
+        if (ip !== lastKnownIP) {
+          searchRecord(ip);
+        } else {
+          if (opts.debug) {
+            utils.debugBlock('IP the same as last time', 'No update needed');
+          }
+        }
+      }
+      catch(err) {
+        if (opts.debug) {
+          utils.debugBlock('Error getting ip from server', err);
+        }
+      }
+    }
+  });
+}
+
+module.exports = checkIpUpdate;
